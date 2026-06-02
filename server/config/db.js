77f60c8e0a102c17a88e6ef7c67151ef0a1c9f4f
Arnav-Bansal2710 <bansalarnavab@gmail.com>
2026-06-02 19:39:17 +0530

    Fix Railway DB port

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    port: Number(process.env.DB_PORT),
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
@@ -11,6 +11,4 @@ const pool = mysql.createPool({
     connectionLimit: 10,
 });
 
-const db = pool.promise();
-
-module.exports = db;
+module.exports = pool.promise();
